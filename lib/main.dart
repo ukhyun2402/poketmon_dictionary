7247c33c4753e830f9a094eef8c398be3950c9a4
wookhyun ha <hyun4911@gmail.com>
2023-03-03 17:17:03 +0900

    to make appbar to dynamic

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:poketmon_dictionary/service/poketmon_service.dart';
+import 'package:poketmon_dictionary/service/ui_service.dart';
 import 'components/poketmon_tile.dart';
 
 void main() {
@@ -18,8 +22,15 @@ class _MainScreenState extends ConsumerState<MainScreen> {
 
   @override
   void initState() {
-    _controller = ScrollController()..addListener(meetEndOfScroll);
+    _controller = ScrollController()..addListener(scrollControllerEvent);
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _controller.removeListener(() {});
+    super.dispose();
   }
 
   @override
@@ -36,10 +47,19 @@ class _MainScreenState extends ConsumerState<MainScreen> {
     );
   }
 
-  void meetEndOfScroll() {
+  void scrollControllerEvent() {
+    final srcollProvider = ref.read(scrollDirectionProvider.notifier);
     if (_controller.offset >= _controller.position.maxScrollExtent &&
         !_controller.position.outOfRange) {
       ref.read(poketmonPaginationProvider.notifier).state++;
+    }
+
+    if (_controller.position.userScrollDirection == ScrollDirection.reverse) {
+      srcollProvider.state = false;
+    }
+
+    if (_controller.position.userScrollDirection == ScrollDirection.forward) {
+      srcollProvider.state = true;
     }
   }
 }
@@ -49,13 +69,53 @@ class MyApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final appBarVisible = ref.watch(scrollDirectionProvider);
     return MaterialApp(
-      theme: ThemeData(fontFamily: 'NotoSansKR'),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("포켓몬 도감"),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'NotoSansKR',
+        brightness: Brightness.light,
+        primaryColor: Colors.white,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          titleTextStyle: TextStyle(
+            color: Colors.black,
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+          ),
         ),
-        body: const MainScreen(),
+      ),
+      home: Scaffold(
+        // appBar: AppBar(title: Consumer(
+        //   builder: (context, re, child) {
+        //     final appBarisVisible = ref.watch(scrollDirectionProvider);
+        //     log(appBarisVisible.toString());
+        //     return AnimatedContainer(
+        //       duration: const Duration(milliseconds: 200),
+        //       height: appBarisVisible ? 0 : 56,
+        //       child: Text("hello world"),
+        //     );
+        //   },
+        // )),
+        // TODO: hide appbar when scroll down
+        body: NestedScrollView(
+          floatHeaderSlivers: true,
+          headerSliverBuilder: (context, innerBoxIsScrolled) {
+            return [
+              Consumer(builder: ((context, ref, child) {
+                final appbarVisible = ref.watch(scrollDirectionProvider);
+                log(appbarVisible.toString());
+                return SliverAppBar(
+                  title: Text("HELLOWORLD"),
+                  floating: appbarVisible,
+                  snap: appbarVisible,
+                  // forceElevated: appbarVisible,
+                );
+              }))
+            ];
+          },
+          body: const MainScreen(),
+        ),
       ),
     );
   }
---
+++ lib/service/ui_service.dart
@@ -0,0 +1,7 @@
+import 'dart:developer';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final scrollDirectionProvider = StateProvider<bool>((ref) {
+  return true;
+});
